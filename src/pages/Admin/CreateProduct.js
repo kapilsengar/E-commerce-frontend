4bd0ac154adf106edea0c7585ffc519c9e066c28
Kapil Sengar <kapilsengar@Kapils-Laptop.local>
2024-08-16 20:16:49 +0530

    changes update

--- src/pages/Admin/CreateProduct.js
+++ src/pages/Admin/CreateProduct.js
@@ -5,6 +5,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { Select } from "antd";
 import { useNavigate } from "react-router-dom";
+import url from "../../backendUrl";
 const { Option } = Select;
 
 const CreateProduct = () => {
@@ -15,13 +16,14 @@ const CreateProduct = () => {
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [quantity, setQuantity] = useState("");
+  // eslint-disable-next-line
   const [shipping, setShipping] = useState("");
   const [photo, setPhoto] = useState("");
 
   //get all category
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("/api/v1/category/get-category");
+      const { data } = await axios.get(url+"/api/v1/category/get-category");
       if (data?.success) {
         setCategories(data?.category);
       }
@@ -47,7 +49,7 @@ const CreateProduct = () => {
       productData.append("photo", photo);
       productData.append("category", category);
       const { data } = axios.post(
-        "/api/v1/product/create-product",
+        url+"/api/v1/product/create-product",
         productData
       );
       if (data?.success) {
